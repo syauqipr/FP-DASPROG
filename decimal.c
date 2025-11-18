649ba74c504c3102a128508877756c3ff987a578
Arkyvv <138296324+Arkyvv@users.noreply.github.com>
2025-11-18 21:04:21 +0000

    progress

--- decimal.c
+++ decimal.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include "kalkulator.h"
 
-int digit_total(float num) {
+int digit_oktal(float num) {
     int result;
     for(int i = 5; i >= 0 ; i--){
         if(num >=  pow(8,i)) {
@@ -11,6 +11,16 @@ int digit_total(float num) {
 
         }
     }
+    return result;
+}
+int digit_heksa(float num){
+    int result = 0;
+    int temp = num;
+    do{
+        temp /= 16;
+        ++result;
+    }while(temp != 0);
+
     return result;
 }
 
@@ -44,16 +54,17 @@ void decimal() {
         }
     }
     
-    printf("-Biner      : ");
+    printf("-Biner        : ");
     for(int i = 0; i < bit; i++){
         printf("%d", biner[i]);
     }
     printf("\n");
 
     //konversi ke Oktal
+
     temp1 = num;
 
-    int digit = digit_total(num);
+    int digit = digit_oktal(num);
     int oktal[digit];
 
     for(int i = 1; i <= digit; i++ ){
@@ -61,11 +72,48 @@ void decimal() {
         temp1 /= 8;
     }
 
-    printf("-Oktal      : ");
+    printf("-Oktal        : ");
     for(int i = 0; i < digit; i++){
         printf("%d", oktal[i]);
     }
     printf("\n");
-    
 
-}
+    // konversi ke heksadesimal
+
+    temp1 = num;
+    digit = digit_heksa(num);
+    int heksa[digit];
+
+    for(int i = digit-1; i >= 0; i--){
+        heksa[i] = temp1 % 16;
+        temp1 /= 16;
+    }
+
+    printf("-Heksadesimal : ");
+    for(int i = 0; i < digit; i++){
+        switch(heksa[i]){
+            case 10:
+                printf("A");
+                continue;
+            case 11:
+                printf("B");
+                continue;
+            case 12:
+                printf("C");
+                continue;
+            case 13:
+                printf("D");
+               continue;
+            case 14:
+                printf("E");
+                continue;
+            case 15:
+                printf("F");
+                continue;
+            default:
+                break;
+        }
+        printf("%d", heksa[i]);
+    }
+    printf("\n");
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
