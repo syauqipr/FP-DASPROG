d3ed07668c3460602cf26c7fed882695b320c790
syauqipr <syauqipr2@gmail.com>
2025-12-10 08:55:38 +0700

    Create daya.c

---
+++ daya.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+void garis() {
+    printf("+------------------------------------------+\n");
+}
+
+int main() {
+    int pilihan = 0; 
+    float V, I, R, P;
+
+    while (pilihan != 4) {
+
+        garis();
+        printf("|        KALKULATOR DAYA LISTRIK           |\n");
+        garis();
+        printf("| Daya (P) memiliki satuan Watt            |\n");
+        printf("| Rumus daya:                              |\n");
+        printf("| 1) P = V x I                             |\n");
+        printf("| 2) P = I^2 x R                           |\n");
+        printf("| 3) P = V^2 / R                           |\n");
+        garis();
+
+        printf("|                  MENU                    |\n");
+        garis();
+        printf("| 1. Hitung dari V dan I                   |\n");
+        printf("| 2. Hitung dari I dan R                   |\n");
+        printf("| 3. Hitung dari V dan R                   |\n");
+        printf("| 4. Keluar                                |\n");
+        garis();
+        printf("| Pilih (1-4): ");
+        scanf("%d", &pilihan);
+        garis();
+
+        if (pilihan == 1) {
+            printf(" Masukkan Tegangan (V) tanpa satuan : ");
+            scanf("%f", &V);
+            printf(" Masukkan Arus (I) tanpa satuan : ");
+            scanf("%f", &I);
+            P = V * I;
+            printf(" Daya (P) = %.2f Watt\n", P);
+        }
+
+        else if (pilihan == 2) {
+            printf(" Masukkan Arus (I) tanpa satuan : ");
+            scanf("%f", &I);
+            printf(" Masukkan Hambatan (R) tanpa satuan : ");
+            scanf("%f", &R);
+            P = I * I * R;
+            printf(" Daya (P) = %.2f Watt\n", P);
+        }
+
+        else if (pilihan == 3) {
+            printf(" Masukkan Tegangan (V) tanpa satuan : ");
+            scanf("%f", &V);
+            printf(" Masukkan Hambatan (R) tanpa satuan : ");
+            scanf("%f", &R);
+            P = (V * V) / R;
+            printf(" Daya (P) = %.2f Watt\n", P);
+        }
+
+        else if (pilihan == 4) {
+            printf("Keluar program...\n");
+        }
+
+        else {
+            printf(" Pilihan tidak valid! Masukkan angka 1-4.\n");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
